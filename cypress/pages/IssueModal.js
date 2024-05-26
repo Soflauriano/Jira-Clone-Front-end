8f1f4c891ca4e0bf10080589c8b04caa25b8ef64
Soflauriano <soraiaohe@gmail.com>
2024-05-26 22:53:14 +0100

    Que dureza

--- cypress/pages/IssueModal.js
+++ cypress/pages/IssueModal.js
@@ -1,121 +1,148 @@
 class IssueModal {
-    constructor() {
-        this.submitButton = 'button[type="submit"]';
-        this.issueModal = '[data-testid="modal:issue-create"]';
-        this.issueDetailModal = '[data-testid="modal:issue-details"]';
-        this.title = 'input[name="title"]';
-        this.issueType = '[data-testid="select:type"]';
-        this.descriptionField = '.ql-editor';
-        this.assignee = '[data-testid="select:userIds"]';
-        this.backlogList = '[data-testid="board-list:backlog"]';
-        this.issuesList = '[data-testid="list-issue"]';
-        this.deleteButton = '[data-testid="icon:trash"]';
-        this.deleteButtonName = "Delete issue";
-        this.cancelDeletionButtonName = "Cancel";
-        this.confirmationPopup = '[data-testid="modal:confirm"]';
-        this.closeDetailModalButton = '[data-testid="icon:close"]';
-    }
+  constructor() {
+    this.submitButton = 'button[type="submit"]';
+    this.issueModal = '[data-testid="modal:issue-create"]';
+    this.issueDetailModal = '[data-testid="modal:issue-details"]';
+    this.title = 'input[name="title"]';
+    this.issueType = '[data-testid="select:type"]';
+    this.descriptionField = ".ql-editor";
+    this.assignee = '[data-testid="select:userIds"]';
+    this.backlogList = '[data-testid="board-list:backlog"]';
+    this.issuesList = '[data-testid="list-issue"]';
+    this.deleteButton = '[data-testid="icon:trash"]';
+    this.deleteButtonName = "Delete issue";
+    this.cancelDeletionButtonName = "Cancel";
+    this.confirmationPopup = '[data-testid="modal:confirm"]';
+    this.closeDetailModalButton = '[data-testid="icon:close"]';
+    this.reporter = '[data-testid="select:reporterId"]';
+    this.priority = '[data-testid="select:priority"]';
+  }
 
-    getIssueModal() {
-        return cy.get(this.issueModal);
-    }
+  getIssueModal() {
+    return cy.get(this.issueModal);
+  }
 
-    getIssueDetailModal() {
-        return cy.get(this.issueDetailModal);
-    }
+  getIssueDetailModal() {
+    return cy.get(this.issueDetailModal);
+  }
 
-    selectIssueType(issueType) {
-        cy.get(this.issueType).click('bottomRight');
-        cy.get(`[data-testid="select-option:${issueType}"]`)
-            .trigger('mouseover')
-            .trigger('click');
+  selectIssueType(issueType) {
+    if (issueType != "Task") {
+      cy.get(this.issueType).click("bottomRight");
+      cy.get(`[data-testid="select-option:${issueType}"]`)
+        .trigger("mouseover")
+        .trigger("click");
     }
+  }
 
-    selectAssignee(assigneeName) {
-        cy.get(this.assignee).click('bottomRight');
-        cy.get(`[data-testid="select-option:${assigneeName}"]`).click();
+  selectAssignee(assigneeName) {
+    if (assigneeName != null) {
+      cy.get(this.assignee).click("bottomRight");
+      cy.get(`[data-testid="select-option:${assigneeName}"]`).click();
     }
+  }
 
-    editTitle(title) {
-        cy.get(this.title).debounced('type', title);
-    }
+  selectReporter(reporterName) {
+    cy.get(this.reporter).click("bottomRight");
+    cy.get(`[data-testid="select-option:${reporterName}"]`).click();
+  }
 
-    editDescription(description) {
-        cy.get(this.descriptionField).type(description);
-    }
+  selectPriority(priorityName) {
+    cy.get(this.priority).click("bottomRight");
+    cy.get(`[data-testid="select-option:${priorityName}"]`).click();
+  }
 
-    createIssue(issueDetails) {
-        this.getIssueModal().within(() => {
-            this.selectIssueType(issueDetails.type);
-            this.editDescription(issueDetails.description);
-            this.editTitle(issueDetails.title);
-            this.selectAssignee(issueDetails.assignee);
-            cy.get(this.submitButton).click();
-        });
-    }
+  editTitle(title) {
+    cy.get(this.title).debounced("type", title);
+  }
 
-    ensureIssueIsCreated(expectedAmountIssues, issueDetails) {
-        cy.get(this.issueModal).should('not.exist');
-        cy.reload();
-        cy.contains('Issue has been successfully created.').should('not.exist');
+  editDescription(description) {
+    cy.get(this.descriptionField).type(description);
+  }
 
-        cy.get(this.backlogList).should('be.visible').and('have.length', '1').within(() => {
-            cy.get(this.issuesList)
-                .should('have.length', expectedAmountIssues)
-                .first()
-                .find('p')
-                .contains(issueDetails.title);
-            cy.get(`[data-testid="avatar:${issueDetails.assignee}"]`).should('be.visible');
-        });
-    }
+  createIssue(issueDetails) {
+    this.getIssueModal().within(() => {
+      this.selectIssueType(issueDetails.type);
+      this.editDescription(issueDetails.description);
+      this.editTitle(issueDetails.title);
+      this.selectAssignee(issueDetails.assignee);
+      this.selectReporter(issueDetails.reporter);
+      this.selectPriority(issueDetails.priority);
 
-    ensureIssueIsVisibleOnBoard(issueTitle) {
-        cy.get(this.issueDetailModal).should('not.exist');
-        cy.reload();
-        cy.contains(issueTitle).should('be.visible');
-    }
+      cy.get(this.submitButton).click();
+    });
+  }
 
-    ensureIssueIsNotVisibleOnBoard(issueTitle) {
-        cy.get(this.issueDetailModal).should('not.exist');
-        cy.reload();
-        cy.contains(issueTitle).should('not.exist');
-    }
+  ensureIssueIsCreated(expectedAmountIssues, issueDetails) {
+    cy.get(this.issueModal).should("not.exist");
+    cy.reload();
+    cy.contains("Issue has been successfully created.").should("not.exist");
 
-    validateIssueVisibilityState(issueTitle, isVisible = true) {
-        cy.get(this.issueDetailModal).should('not.exist');
-        cy.reload();
-        cy.get(this.backlogList).should('be.visible');
-        if (isVisible)
-            cy.contains(issueTitle).should('be.visible');
-        if (!isVisible)
-            cy.contains(issueTitle).should('not.exist');
-    }
+    cy.get(this.backlogList)
+      .should("be.visible")
+      .and("have.length", "1")
+      .within(() => {
+        cy.get(this.issuesList)
+          .should("have.length", expectedAmountIssues)
+          .first()
+          .find("p")
+          .contains(issueDetails.title);
+        if (issueDetails.assignee != null) {
+          cy.get(`[data-testid="avatar:${issueDetails.assignee}"]`).should(
+            "be.visible"
+          );
+        }
+      });
+  }
 
-    clickDeleteButton() {
-        cy.get(this.deleteButton).click();
-        cy.get(this.confirmationPopup).should('be.visible');
-    }
+  ensureIssueIsVisibleOnBoard(issueTitle) {
+    cy.get(this.issueDetailModal).should("not.exist");
+    cy.reload();
+    cy.contains(issueTitle).should("be.visible");
+  }
 
-    confirmDeletion() {
-        cy.get(this.confirmationPopup).within(() => {
-            cy.contains(this.deleteButtonName).click();
-        });
-        cy.get(this.confirmationPopup).should('not.exist');
-        cy.get(this.backlogList).should('be.visible');
-    }
+  ensureIssueIsNotVisibleOnBoard(issueTitle) {
+    cy.get(this.issueDetailModal).should("not.exist");
+    cy.reload();
+    cy.contains(issueTitle).should("not.exist");
+  }
 
-    cancelDeletion() {
-        cy.get(this.confirmationPopup).within(() => {
-            cy.contains(this.cancelDeletionButtonName).click();
-        });
-        cy.get(this.confirmationPopup).should('not.exist');
-        cy.get(this.issueDetailModal).should('be.visible');
-    }
+  validateIssueVisibilityState(issueTitle, isVisible = true) {
+    cy.get(this.issueDetailModal).should("not.exist");
+    cy.reload();
+    cy.get(this.backlogList).should("be.visible");
+    if (isVisible) cy.contains(issueTitle).should("be.visible");
+    if (!isVisible) cy.contains(issueTitle).should("not.exist");
+  }
 
-    closeDetailModal() {
-        cy.get(this.issueDetailModal).get(this.closeDetailModalButton).first().click();
-        cy.get(this.issueDetailModal).should('not.exist');
-    }
+  clickDeleteButton() {
+    cy.get(this.deleteButton).click();
+    cy.get(this.confirmationPopup).should("be.visible");
+  }
+
+  confirmDeletion() {
+    cy.get(this.confirmationPopup).within(() => {
+      cy.contains(this.deleteButtonName).click();
+    });
+    cy.get(this.confirmationPopup).should("not.exist");
+    cy.get(this.backlogList).should("be.visible");
+  }
+
+  cancelDeletion() {
+    cy.get(this.confirmationPopup).within(() => {
+      cy.contains(this.cancelDeletionButtonName).click();
+    });
+    cy.get(this.confirmationPopup).should("not.exist");
+    cy.get(this.issueDetailModal).should("be.visible");
+  }
+
+  closeDetailModal() {
+    cy.get(this.issueDetailModal)
+      .get(this.closeDetailModalButton)
+      .first()
+      .click();
+    cy.get(this.issueDetailModal).should("not.exist");
+  }
 }
 
 export default new IssueModal();
